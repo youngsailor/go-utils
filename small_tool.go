0c5cd4431c7dbadad4d5efe6e14762b5905d5033
youngsailor <591245853@qq.com>
2022-11-26 19:58:57 +0800

    增加可以获取剩余时间的timer

--- small_tool.go
+++ small_tool.go
@@ -87,3 +87,26 @@ func FilterEmoji(content string) string {
 	}
 	return newContent
 }
+
+// SecondsTimer 可以获取剩余时间的timer
+type SecondsTimer struct {
+	timer *time.Timer
+	end   time.Time
+}
+
+func NewSecondsTimer(t time.Duration) *SecondsTimer {
+	return &SecondsTimer{time.NewTimer(t), time.Now().Add(t)}
+}
+
+func (s *SecondsTimer) Reset(t time.Duration) {
+	s.timer.Reset(t)
+	s.end = time.Now().Add(t)
+}
+
+func (s *SecondsTimer) Stop() {
+	s.timer.Stop()
+}
+
+func (s *SecondsTimer) TimeRemaining() time.Duration {
+	return s.end.Sub(time.Now())
+}
